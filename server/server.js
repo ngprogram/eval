65de1f440674afbedb5ffc363388738adb4af8b4
Alex Zai <azai91@gmail.com>
2015-02-27 19:05:13 -0800

    (feat) add send email and passport routes to routeHandler

--- server/server.js
+++ server/server.js
@@ -1,22 +1,9 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
-var cors = require('cors');
-var session = require('express-session');
-var path = require('path');
 
 var routeHandler = require('./routeHandler');
 
 var app = express();
 
-app.use(session({
-  secret: 'secretttt',
-   resave: false,
-   saveUninitialized: true
-   }));
-app.use(bodyParser.json());
-app.use(cors());
-app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/../client/')));
 
 var port = process.env.PORT || 8000;
