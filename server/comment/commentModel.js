572d9d80df8b528273b8b730c831aee5e047fb9a
Alex Zai <azai91@gmail.com>
2015-02-27 16:01:58 -0800

    (feat) create comment and user model

---
+++ server/comment/commentModel.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose'),
+    Schema   = mongoose.Schema;
+var sentimentAnalyzer = require('../util/sentimentAnalyzer');
+
+// define message schema
+var commentSchema = new Schema({
+  companyId: {type: String, required: true}, //same as id from user
+  comment: {type: String, required: true},
+  employee_name: {type: String, default: null },
+  phone_number: {type: String},
+  date: {type: Date, required: true},
+  score: {type: Number} // from sentiment anaylsis
+});
+
+commentSchema.pre('save', function(next) {
+  var comment = this;
+  comment.score = sentimentAnalyzer.generateSentiment(comment.comment, function(err, score) {
+    comment.score = score;
+    next();
+  })
+});
+
+module.exports = mongoose.model('Item', commentSchema);
