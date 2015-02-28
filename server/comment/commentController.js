d6ecbab0a4126988e9c59b4b9aa6fd53c1d86a6c
Caroline Wong <carowong5@gmail.com>
2015-02-27 22:52:32 -0800

    (feat) comment objects created

--- server/comment/commentController.js
+++ server/comment/commentController.js
@@ -6,11 +6,12 @@ commentController.saveComment = saveComment;
 commentController.getCommentByCompanyId = getCommentByCompanyId;
 
 function saveComment(comment, callback) {
+  console.log('saving',comment);
   Comment.create(comment, callback);
 }
 
 function getCommentByCompanyId(companyId, callback) {
   Comment.find({companyId: companyId}, callback);
-}
+} 
 
 module.exports = commentController;
