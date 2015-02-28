095065f80699427d0217a1ee824eca56b239f5de
Alex Zai <azai91@gmail.com>
2015-02-28 09:41:37 -0800

    (feat) passes basic tests

--- test/sentimentAnalyzerSpec.js
+++ test/sentimentAnalyzerSpec.js
@@ -4,7 +4,7 @@ var expect = require('chai').expect;
 describe('Sentiment Analyzer', function() {
   it('should return a corrected comment', function(done) {
     var comment = "that was great";
-    sentimentAnalyzer(comment, function(score) {
+    sentimentAnalyzer(comment, function(err, score) {
       expect(score).to.exist;
       expect(score).to.be.gte(0);
       done();
