8216bf09215dac2f76f34247c51c8dcd3d48a0c3
Alex Zai <azai91@gmail.com>
2015-02-27 16:30:24 -0800

    (fix) add sentiment analyzer test and fix errors

---
+++ test/sentimentAnalyzerSpec.js
@@ -0,0 +1,13 @@
+var sentimentAnalyzer = require('../server/util/sentimentAnalyzer');
+var expect = require('chai').expect;
+
+describe('Sentiment Analyzer', function() {
+  it('should return a corrected comment', function(done) {
+    var comment = "that was great";
+    sentimentAnalyzer(comment, function(score) {
+      expect(score).to.exist;
+      expect(score).to.be.gte(0);
+      done();
+    });
+  });
+});
