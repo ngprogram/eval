52ff90601265255267a0f230fb5310b4cea6316a
Caroline Wong <carowong5@gmail.com>
2015-02-27 23:59:07 -0800

    change to http server (explicit)

--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
 var express = require('express');
-
+var http = require('http');
 var routeHandler = require('./routeHandler');
 var webApiRouter = require('./textRoutes/webApiRouter');
 var webhook = webApiRouter.webhook;
-
 var app = express();
+app.set('port', process.env.PORT || 8000);
 
-var port = process.env.PORT || 8000;
-console.log('Server listening on ' + port);
+var server = http.createServer(app);
+server.listen(app.use(routeHandler).get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
 
-app.use(routeHandler).listen(port);
+// var port = process.env.PORT || 8000;
+// console.log('Server listening on ' + port);
 
-module.exports = app;
+// app.use(routeHandler).listen(port);
+
+module.exports = server;
