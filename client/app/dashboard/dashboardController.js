2d3bb5136ffe1a5779d0b52ba3a32c988ed48e2d
Jonathan Ng <jon7ng@gmail.com>
2015-02-28 15:36:09 -0800

    (fix) Add sample Data and buttons

--- client/app/dashboard/dashboardController.js
+++ client/app/dashboard/dashboardController.js
@@ -4,10 +4,17 @@ angular
 
   DashboardController.$inject = [
     '$scope',
+    'DashboardFactory',
     'AuthFactory',
   ];
 
-  function DashboardController ($scope, AuthFactory) {
+  function DashboardController ($scope, DashboardFactory, AuthFactory) {
+    console.log('sampleData', sampleData);
+
+    var sortedData = DashboardFactory.sortByTime(sampleData);
+    console.log('sortedData', sortedData);
+    var dataProcessedTime = DashboardFactory.changeTimeFormat(sortedData);
+    console.log('dataProcessedTime', dataProcessedTime);
     $scope.averageSentiment = '80';
     $scope.responseCount = '140';
 
@@ -22,7 +29,10 @@ angular
             columns: [
               ['data', 90, 50, 40, -94, 30, 40, 90],
             ],
-            type: 'bar'
+            type: 'bar',
+            colors: {
+                'data': function(d) { return d.value < 0 ? '#FF0000' : '#0000FF'; }
+            }
           },
           bar: {
             width: {
@@ -93,23 +103,49 @@ angular
     $scope.changeSentimentChartView = function(time) {
       switch (time) {
         case 'day':
+          var newData = DashboardFactory.filterByDay(dataProcessedTime);
           $scope.sentimentChart.load({
-              categories: ['1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm', '12am']
+            columns: [
+              ['data', newData[0], newData[1], newData[2], newData[3], newData[4], newData[5], newData[6],
+              newData[7], newData[8], newData[9], newData[10], newData[11], newData[12], newData[13], newData[14],
+              newData[15], newData[16], newData[17], newData[18], newData[19], newData[20], newData[21], newData[22],
+              newData[23]]
+            ],
+            categories: ['12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm']
           });
           break;
         case 'week':
+          var newData = DashboardFactory.filterByWeek(dataProcessedTime);
+          console.log('newData', newData)
           $scope.sentimentChart.load({
-              categories: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+            columns: [
+              ['data', newData[0], newData[1], newData[2], newData[3], newData[4], newData[5], newData[6]]
+            ],
+            categories: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
           });
           break;
         case 'month':
+          var newData = DashboardFactory.filterByMonth(dataProcessedTime);
           $scope.sentimentChart.load({
-              categories: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+            columns: [
+              ['data', newData[0], newData[1], newData[2], newData[3], newData[4], newData[5], newData[6], newData[7], newData[8], newData[9], newData[10], newData[11]]
+            ],
+            categories: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
           });
           break;
         case 'year':
+          var newData = DashboardFactory.filterByYear(dataProcessedTime);
+          var arrayOfYears = Object.keys(newData);
+          var newDataProcessed = ['data'];
+          for (var i = 0; i < arrayOfYears.length; i++) {
+            newDataProcessed.push(newData[arrayOfYears[i]]);
+          }
+          console.log('arrayOfYears', arrayOfYears);
           $scope.sentimentChart.load({
-              categories: ['2010', '2011', '2012', '2013', '2014', '2015']
+            columns: [
+              newDataProcessed
+            ],
+            categories: arrayOfYears
           });
           break;
       }
@@ -119,22 +155,22 @@ angular
       switch (time) {
         case 'day':
           $scope.responsesChart.load({
-              categories: ['1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm', '12am']
+            categories: ['1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm', '12am']
           });
           break;
         case 'week':
           $scope.responsesChart.load({
-              categories: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+            categories: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
           });
           break;
         case 'month':
           $scope.responsesChart.load({
-              categories: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+            categories: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
           });
           break;
         case 'year':
           $scope.responsesChart.load({
-              categories: ['2010', '2011', '2012', '2013', '2014', '2015']
+            categories: ['2010', '2011', '2012', '2013', '2014', '2015']
           });
           break;
       }
--- client/app/dashboard/dashboardServices.js
+++ client/app/dashboard/dashboardServices.js
@@ -8,5 +8,111 @@ angular
   ];
 
   function DashboardFactory ($http, AuthFactory) {
+    var factory = {};
+    factory.sortByTime = sortByTime;
+    factory.changeTimeFormat = changeTimeFormat;
+    factory.filterByDay = filterByDay;
+    factory.filterByWeek = filterByWeek;
+    factory.filterByMonth = filterByMonth;
+    factory.filterByYear = filterByYear;
 
+    function filterByDay(data) {
+      // console.log('filterByWeek', data);
+      var processedData = {};
+      for (var i = 0; i < data.length; i ++) {
+        var hour = data[i].date.getHours();
+        if (processedData[hour]) {
+          processedData[hour][0] += data[i].score;
+          processedData[hour][1]++;
+        } else {
+          processedData[hour] = [data[i].score, 1];
+        }
+      }
+
+      for (var hour in processedData) {
+        processedData[hour] = ((processedData[hour][0]/processedData[hour][1]) * 10000);
+      }
+      console.log('processedData', processedData)
+      return processedData;
+    }
+
+    function filterByWeek(data) {
+      // console.log('filterByWeek', data);
+      var processedData = {};
+      for (var i = 0; i < data.length; i ++) {
+        var day = data[i].date.getDay();
+        if (processedData[day]) {
+          processedData[day][0] += data[i].score;
+          processedData[day][1]++;
+        } else {
+          processedData[day] = [data[i].score, 1];
+        }
+      }
+
+      for (var day in processedData) {
+        processedData[day] = ((processedData[day][0]/processedData[day][1]) * 10000);
+      }
+      console.log('processedData', processedData)
+      return processedData;
+    }
+
+    function filterByMonth(data) {
+      // console.log('filterByWeek', data);
+      var processedData = {};
+      for (var i = 0; i < data.length; i ++) {
+        var month = data[i].date.getMonth();
+        if (processedData[month]) {
+          processedData[month][0] += data[i].score;
+          processedData[month][1]++;
+        } else {
+          processedData[month] = [data[i].score, 1];
+        }
+      }
+
+      for (var month in processedData) {
+        processedData[month] = ((processedData[month][0]/processedData[month][1]) * 10000);
+      }
+      console.log('processedData', processedData)
+      return processedData;
+    }
+
+    function filterByYear(data) {
+      // console.log('filterByWeek', data);
+      var processedData = {};
+      for (var i = 0; i < data.length; i ++) {
+        var year = data[i].date.getFullYear();
+        if (processedData[year]) {
+          processedData[year][0] += data[i].score;
+          processedData[year][1]++;
+        } else {
+          processedData[year] = [data[i].score, 1];
+        }
+      }
+
+      for (var year in processedData) {
+        processedData[year] = ((processedData[year][0]/processedData[year][1]) * 10000);
+      }
+      console.log('processedData', processedData)
+      return processedData;
+    }
+
+    function sortByTime(data) {
+      var sortedData = data.sort(compareByDateMilliseconds);
+
+      function compareByDateMilliseconds(a, b) {
+        return a.date - b.date;
+      }
+
+      return sortedData;
+    }
+
+    function changeTimeFormat(data) {
+      return _.map(data, function(datum) {
+        datum.date = new Date(datum.date);
+        // console.log('changeTimeFormatFunction', datum);
+        return datum;
+      });
+    }
+
+    return factory;
   }
