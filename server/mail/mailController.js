9aacdd39f25261da7b855beb3f2827a55613a4a3
Alex Zai <azai91@gmail.com>
2015-02-27 18:03:48 -0800

    (feat) add mail controller

---
+++ server/mail/mailController.js
@@ -0,0 +1,139 @@
+var mailCreator = require('./mailCreator');
+var User = require('../user/userModel');
+var tempUser = require('../user/tempUserModel');
+var config = require('config');
+var sendGridInfo = config.get('mail');
+var sendgrid  = require('sendgrid')(sendGridInfo.api_user, sendGridInfo.api_key);
+
+var mailController = {};
+
+mailController.sendConfirmationEmail = sendConfirmationEmail;
+mailController.verficationOfAccount = verficationOfAccount;
+mailController.sendForgotPasswordEmail = sendForgotPasswordEmail;
+mailController.verifyResetCode = verifyResetCode;
+mailController.resetPassword = resetPassword;
+
+function sendConfirmationEmail(req, res, next) {
+    var tempAccount = req.body;
+    var email = tempAccount.email;
+    var rand = Math.floor((Math.random() * 1000000000000))
+    var host=req.get('host');
+    tempAccount.rand = rand;
+    tempUser.create(tempAccount, function(err, createdAccount) {
+        if (!err) {
+            var link="http://"+req.get('host')+"/verify?id="+rand;
+            sendgrid.send(mailCreator.createVerificationEmail(email, link), function(err, json) {
+                if (err) {
+                    return console.error(err);
+                } else {
+                    res.sendStatus(200);
+                }
+            });
+        }
+    });
+};
+
+
+function verficationOfAccount(req, res, next) {
+    tempUser.findOne({rand: req.query.id}, function(err, foundTemp) {
+        if(!err) {
+            console.log('verified');
+            var verifiedAccount = {
+                username: foundTemp.username,
+                email: foundTemp.email,
+                password: foundTemp.password
+            };
+
+            req.body = verifiedAccount;
+            foundTemp.remove();
+            next();
+        }
+        else {
+            console.log("email is not verified");
+            res.redirect('/');
+        }
+    })
+}
+
+function sendForgotPasswordEmail(req, res, next) {
+    var email = req.body.email;
+    var rand = Math.floor((Math.random() * 1000000000000))
+    host=req.get('host');
+
+    var link="http://"+req.get('host')+"/reset/" + rand;
+    User.findOne({email: email}, function(err, foundUser) {
+        var forgotEmailObj = {
+            isValid: false
+        };
+        if (err || !foundUser) {
+            res.send(forgotEmailObj);
+        }
+        if (foundUser) {
+
+            var forgotAccount = {
+                rand: rand,
+                email: email
+            };
+            tempUser.create(forgotAccount, function(err, createdTempAccount) {
+                var username = foundUser.username;
+
+                sendgrid.send(mailCreator.createForgottenPasswordEmail(email, username, link), function(err, json) {
+                    if (err) {
+                        return console.error(err);
+                    } else {
+                        forgotEmailObj.isValid = true;
+                        res.send(forgotEmailObj);
+                    }
+                });
+            });
+        }
+    });
+}
+
+function verifyResetCode(req, res, next) {
+    var resetObj = {
+        isValid: false
+    };
+
+    tempUser.findOne({rand: req.body.resetId}, function(err, foundTempUser) {
+        if (err) {
+
+        }
+        if (foundTempUser) {
+           resetObj.isValid = true;
+        }
+        res.send(resetObj);
+    });
+
+}
+
+function resetPassword(req, res, next) {
+    var newPassword = req.body.password;
+
+    tempUser.findOne({rand: req.body.resetId}, function(err, foundTempUser) {
+        if (!err) {
+            User.findOne({email: foundTempUser.email}, function(err, foundUser) {
+                if (!err) {
+                    console.log('foundUser', foundUser);
+                    foundUser.password = newPassword;
+                    foundUser.save(function(err) {
+                        req.body.username = foundUser.username;
+                        req.body.password = newPassword;
+                        foundTempUser.remove();
+                        next();
+                    });
+                }
+                if (err) {
+                    console.log('error resetting password');
+                }
+
+            });
+        }
+    });
+}
+
+module.exports = mailController;
+
+
+
+
---
+++ server/mail/mailCreator.js
@@ -0,0 +1,31 @@
+var messageContent = {
+  to:       'geteval.com@gmail.com',
+  from:     'geteval.com@gmail.com',
+  subject:  '',
+  text:     ''
+};
+
+var mailCreator = {};
+mailCreator.createVerificationEmail = createVerificationEmail;
+mailCreator.createForgottenPasswordEmail = createForgottenPasswordEmail;
+
+function createVerificationEmail(email, link) {
+    var text = "Please click the following link to validate your email and sign in:\n\n" + link + "\n";
+    var subject = "Eval Verification Email";
+    messageContent.text = text;
+    messageContent.subject = subject;
+    messageContent.to = email;
+
+    return messageContent;
+}
+
+function createForgottenPasswordEmail(email, username, link) {
+    var text = "Please visit the following link to reset your password:\n\n" + link + "\n";
+    var subject = "Eval Forgotten Password Email";
+    messageContent.text = text;
+    messageContent.subject = subject;
+    messageContent.to = email;
+    return messageContent;
+}
+
+module.exports = mailCreator;
