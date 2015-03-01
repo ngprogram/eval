0099ff2cd971505c72c2a2d77d3366a4ad182570
Alex Zai <azai91@gmail.com>
2015-02-28 17:18:04 -0800

    (feat) signup works

--- client/app/app.js
+++ client/app/app.js
@@ -3,13 +3,20 @@ angular
     'eval.dashboard',
     // 'eval.employees',
     'eval.auth',
-    // 'eval.navbar',
+    'eval.navbar',
     'ui.router'
   ])
 .config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function($stateProvider, $urlRouterProvider, $httpProvider) {
   // $urlRouterProvider.otherwise('/login');
 
   $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'app/auth/login.html',
+      resolve: {
+        loggedIn: checkLoggedIn
+      }
+    })
     .state('login', {
       url : '/login',
       templateUrl: 'app/auth/login.html',
@@ -24,11 +31,13 @@ angular
       url: '/dashboard',
       templateUrl: 'app/dashboard/dashboard.html',
       controller: 'DashboardController'
+      // add resolve later
    })
    .state('employees', {
       url: '/employees',
       templateUrl: 'app/employees/employees.html',
       controller: 'EmployeesController'
+      // add resolve later
    });
 
   //send base url to either login or dashboard depending if user is authenticated
@@ -37,7 +46,7 @@ angular
     if (AuthFactory.isLoggedIn()) {
       $state.go('dashboard');
     } else {
-      $state.go('login');      
+      $state.go('login');
     }
   }]);
 
@@ -45,6 +54,28 @@ angular
   // of interceptors. Think of it like middleware for your ajax calls
   //$httpProvider.interceptors.push('AttachTokens');
 }])
+
+function checkLoggedIn($q, $timeout, $http, $location, $rootScope) {
+
+  var deferred = $q.defer();
+
+  $http.get('/loggedin')
+    .success(function(user) {
+      console.log(user);
+    if (user !== '0') {
+      $rootScope.currentUser = user;
+      console.log('signedIn');
+      $timeout(deferred.resolve, 0);
+    } else {
+      $rootScope.currentUser = undefined;
+      $timeout(deferred.resolve, 0);
+    }
+  }).error(function(err) {
+      $location.path('/');
+  });
+
+  return deferred.promise;
+};
 
 
 
