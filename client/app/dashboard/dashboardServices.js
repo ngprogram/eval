83e670ee8ee70e653e49162998335dde5ed4e45e
Jonathan Ng <jon7ng@gmail.com>
2015-03-01 00:31:55 -0800

    (fix) dashboard work

--- client/app/dashboard/dashboardServices.js
+++ client/app/dashboard/dashboardServices.js
@@ -1,13 +1,19 @@
 angular
   .module('eval.dashboard', [])
-  .factory('DashboardFactory', DashboardFactory);
+  .factory('DashboardSentimentFactory', DashboardSentimentFactory)
+  .factory('DashboardCommentsFactory', DashboardCommentsFactory);
 
-  DashboardFactory.$inject = [
+  DashboardSentimentFactory.$inject = [
     '$http',
     'AuthFactory',
   ];
 
-  function DashboardFactory ($http, AuthFactory) {
+  DashboardCommentsFactory.$inject = [
+    '$http',
+    'AuthFactory',
+  ];
+
+  function DashboardSentimentFactory ($http, AuthFactory) {
     var factory = {};
     factory.sortByTime = sortByTime;
     factory.changeTimeFormat = changeTimeFormat;
@@ -132,6 +138,188 @@ angular
       }
       console.log('processedData', processedData)
       return [processedData, averageSentiment];
+    }
+
+    function sortByTime(data) {
+      var sortedData = data.sort(compareByDateMilliseconds);
+
+      function compareByDateMilliseconds(a, b) {
+        return a.date - b.date;
+      }
+
+      return sortedData;
+    }
+
+    function changeTimeFormat(data) {
+      return _.map(data, function(datum) {
+        datum.date = new Date(datum.date);
+        // console.log('changeTimeFormatFunction', datum);
+        return datum;
+      });
+    }
+
+    function isWithinPastDay(date) {
+      var inPastDay = false;
+      //get now date
+      var now = new Date();
+      // console.log('now', now);
+      //get the month in the year
+      var year = now.getFullYear();
+      var month = now.getMonth();
+      var date = now.getDate();
+      // console.log(year, month, date);
+      var nearestMidnightDateAtMidnight = new Date(year, month, date);
+      // console.log('nearestMidnightDateAtMidnight', nearestMidnightDateAtMidnight);
+      //compare if the value is greater than this one above
+      if (date > nearestMidnightDateAtMidnight) {
+        inPastDay = true;
+      }
+      return inPastDay;
+    }
+
+    function isWithinPastWeek(date) {
+      var inPastWeek = false;
+      //get now date
+      var now = new Date();
+      // console.log('now', now);
+      //get the day of the week
+      var day = now.getDay();
+      //change to milliseconds
+      var nowDateInMilliseconds = Date.parse(now);
+      //subtract milliseconds * day
+      var nearestSundayInMilliseconds = nowDateInMilliseconds - day * 86400000; //86400000 milliseconds in a day
+      //subtract the right amount of time to get to a sunday
+      var nearestSundayDate = new Date(nearestSundayInMilliseconds);
+      // console.log('nearestSundayDate', nearestSundayDate)
+      //generate new date using that info and 0,0,0
+      var sundayYear = nearestSundayDate.getFullYear();
+      // console.log(sundayYear);
+      var sundayMonth = nearestSundayDate.getMonth();
+      // console.log(sundayMonth);
+      var sundayDate = nearestSundayDate.getDate();
+      // console.log(sundayMonth);
+      var nearestSundayDateAtMidnight = new Date(sundayYear, sundayMonth, sundayDate);
+      // console.log('nearestSundayDateAtMidnight', nearestSundayDateAtMidnight);
+      //compare if the value is greater than this one above
+      if (date > nearestSundayDateAtMidnight) {
+        inPastWeek = true;
+      }
+      return inPastWeek;
+    }
+
+    function isWithinPastMonth(date) {
+      var inPastMonth = false;
+      //get now date
+      var now = new Date();
+      // console.log('now', now);
+      //get the month in the year
+      var year = now.getFullYear();
+      var month = now.getMonth();
+      // console.log(year);
+      var nearestFirstDayOfMonthAtMidnight = new Date(year, month, 1);
+      console.log('nearestFirstDayOfMonthAtMidnight', nearestFirstDayOfMonthAtMidnight);
+      //compare if the value is greater than this one above
+      if (date > nearestFirstDayOfMonthAtMidnight) {
+        inPastMonth = true;
+      }
+      return inPastMonth;
+    }
+
+    function isWithinPastYear(date) {
+      var inPastYear = false;
+      //get now date
+      var now = new Date();
+      // console.log('now', now);
+      //get the month in the year
+      var year = now.getFullYear();
+      // console.log(year);
+      var nearestJanuaryDateAtMidnight = new Date(year, 0, 1);
+      // console.log('nearestJanuaryDateAtMidnight', nearestJanuaryDateAtMidnight);
+      //compare if the value is greater than this one above
+      if (date > nearestJanuaryDateAtMidnight) {
+        inPastYear = true;
+      }
+      return inPastYear;
+    }
+
+    return factory;
+  }
+
+  function DashboardCommentsFactory ($http, AuthFactory) {
+    var factory = {};
+    factory.sortByTime = sortByTime;
+    factory.changeTimeFormat = changeTimeFormat;
+    factory.filterByDay = filterByDay;
+    factory.filterByWeek = filterByWeek;
+    factory.filterByMonth = filterByMonth;
+    factory.filterByYear = filterByYear;
+
+    function filterByDay(data) {
+      // console.log('filterByWeek', data);
+      var processedData = {};
+      for (var i = 0; i < data.length; i ++) {
+        var hour = data[i].date.getHours();
+        if (processedData[hour] && isWithinPastDay(data[i].date)) {
+          processedData[hour]++;
+        } else {
+          processedData[hour] = 1;
+        }
+      }
+      console.log('pre procsssedData', processedData);
+
+      return processedData;
+    }
+
+    function filterByWeek(data) {
+      // console.log('filterByWeek', data);
+      var processedData = {};
+      for (var i = 0; i < data.length; i ++) {
+        // console.log('MEMEMMEE', data[i].date)
+        var day = data[i].date.getDay();
+        if (processedData[day] && isWithinPastWeek(data[i].date)) {
+          processedData[day]++;
+        } else {
+          processedData[day] = 1;
+        }
+      }
+
+      // console.log('processedData', processedData)
+      return processedData;
+    }
+
+    function filterByMonth(data) {
+      // console.log('filterByWeek', data);
+      var processedData = {};
+      for (var i = 0; i < data.length; i ++) {
+        var month = data[i].date.getMonth();
+        console.log(isWithinPastYear(data[i].date));
+        if (isWithinPastYear(data[i].date)) {
+          if (processedData[month]) {
+            processedData[month]++;
+          } else {
+            processedData[month] = 1;
+          }
+        }
+      }
+
+      // console.log('processedData', processedData)
+      return processedData;
+    }
+
+    function filterByYear(data) {
+      // console.log('filterByWeek', data);
+      var processedData = {};
+      for (var i = 0; i < data.length; i ++) {
+        var year = data[i].date.getFullYear();
+        if (processedData[year]) {
+          processedData[year]++;
+        } else {
+          processedData[year] = 1;
+        }
+      }
+
+      // console.log('processedData', processedData)
+      return processedData;
     }
 
     function sortByTime(data) {
